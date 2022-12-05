717bab6b6a913f7d3c22c6323f5a04926b1b9cc1
William Johnson <wnj@tutamail.com>
2022-12-04 22:40:29 -0500

    Update textCapture.py
    Linux can figure it out

--- textCapture.py
+++ textCapture.py
@@ -1,17 +1,20 @@
-import tempfile
-import json
+import tempfile, os, json
 from PIL import Image
 from pytesseract import pytesseract
 
 global image_counter
 
-with open('config.json', 'r') as f:
-    data = json.load(f)
+try:
+    if os.name == 'nt':
+        with open('config.json', 'r') as f:
+            data = json.load(f)
 
-pytesseract.tesseract_cmd = data['executable_path']
+    pytesseract.tesseract_cmd = data['executable_path']
+except:
+    print('Warning: No path found to tesseract. Proceding anyway.')
+    pass
 
-def get_image(image, calls=0):
+def get_image(image):
     with tempfile.TemporaryFile() as fp:
         image.save(f'{fp.name}.png')
-        text = pytesseract.image_to_string(Image.open(f'{fp.name}.png'))
-        print(text)
+        return pytesseract.image_to_string(Image.open(f'{fp.name}.png'))
